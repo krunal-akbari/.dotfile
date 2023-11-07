4836ad383d7c2c0b1960ebc4d7c5260f521e6826
kruanl233 <krunalakbari233@gmail.com>
2023-11-07 21:40:28 +0530

    some of file has been added and install script for installing all

--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
-myarr=("zsh" "tmux" "bin" "alacritty")
+git remote set-url origin git@github.com:krunal-akbari/.dotfile.git
+git submodule update --init --recursive
+git pull --recurse-submodules
+myarr=("zsh" "tmux" "bin" "alacritty" "i3")
 for i in "${myarr[@]}"
 do
+
+    stow -D $i
     echo "stowing $i"
     stow $i
     echo "done"
